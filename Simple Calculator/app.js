38c1d9ea97881000b095ca9ec019652252603ee9
realsaleemdev <saleembinyousaf006@gmail.com>
2026-02-28 17:05:50 +0500

    Add files via upload

---
+++ Simple Calculator/app.js
@@ -0,0 +1,12 @@
+// Step 1: Pehla number lena
+let num1 = Number(prompt("Enter first number:"));
+
+// Step 2: Doosra number lena
+let num2 = Number(prompt("Enter second number:"));
+
+// Step 3: Sum nikalna
+let sum = num1 + num2;
+
+// Step 4: Result show karna
+alert("Sum is: " + sum);
+
